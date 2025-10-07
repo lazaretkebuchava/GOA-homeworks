7db8df614d863909f50a959cc877b194c5d72465
lazaretkebuchava <lazaretyebuchava@gmail.com>
2025-10-07 17:49:55 +0400

    commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,14 @@
+#2
+#parentheses - ფრჩხილები,
+#quotes - ბრჭყალები,
+
+
+#3
+name_surname = "demetre epkhoshvili"
+print(name_surname)
+
+name_surname = "luka tsiklauri"
+print(name_surname)
+
+name_surname = "saba shubitidze"
+print(name_surname)
