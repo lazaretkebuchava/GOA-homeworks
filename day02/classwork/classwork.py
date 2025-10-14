02be1e062872bcc3e10a25042bb51dc00d8ca46b
lazaretkebuchava <lazaretyebuchava@gmail.com>
2025-10-14 13:33:49 +0400

    commit

---
+++ day02/classwork/classwork.py
@@ -0,0 +1,40 @@
+#1 ცვლადი(variable) ვიყენებთ მონაცემის შესანახად.
+
+# N2
+food = "khinkhali"
+print(food)
+
+#N3
+print(5 + 10)
+print(2.5)
+print("lazare")
+
+#N4
+sport = "football"
+print(sport)
+
+#N5
+print("lazare"+"tkebuchava")
+
+#N6
+print("house" * 3)
+#ეს კოდი გამოიტანს სიტყვა "house"-ს 3-ჯერ რადგან ვამრავლებთ 3 ზე
+
+#N7
+
+# სწორია
+print("this is string")
+# ან
+print("this is string")
+
+
+#N8 
+# არასწორია
+lesson = "math"
+print("lesson")
+# ტერმინალში სტრინგი "math" იმიტომ არ გამოდის რომ არაა საწიჭირო პრინტის გვერდზე lesson-ის ბრჭყალებში ჩასმა
+
+
+#სწორია
+lesson = "math"
+print(lesson)
---
+++ day02/homework/homework.py
@@ -0,0 +1,52 @@
+# 1
+# კონკატენაცია - სტრინგების ერთმანეთზე მიმატება.
+
+
+# 2
+# string
+name = "giorgi taryashvili"
+print(name)
+
+# integer
+num = 15
+print(num)
+
+# float
+num2 = 12.1
+print(num2)
+
+
+# 3
+num = 10
+num1 = 2
+
+print(num + num1) 
+# 12
+
+print(num - num1)
+# 8
+
+print(num * num1)
+# 20
+
+print(num / num1)
+# 5.0
+
+
+# 4
+print(("lazare ")*20)
+
+
+# 5
+# სტრინგის მიმატება ინტეჯერზე არ შეიძლება და გამოიტანს ერორს, ხოლო გამრავლება შეიძლება.
+
+
+# 6
+name = "student"
+print(name + name)
+# გამოიტანს studentstudent
+
+number = 23
+number1 = "4"
+print(number + number1)
+# გამოიტანს ერორს.
