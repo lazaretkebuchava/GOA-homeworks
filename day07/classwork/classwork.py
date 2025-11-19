14ba92dc1d8875365be51a5b882fb1a03ad6c99f
lazaretkebuchava <lazaretyebuchava@gmail.com>
2025-11-19 22:54:13 +0400

    commit

---
+++ day07/classwork/classwork.py
@@ -0,0 +1,53 @@
+# 1
+
+# input() - იღებს ტექსტურ ინფორმაციას მომხმარებლისგან.
+
+# output() - გამოიტანს მომხმარებლისგან მიღებულ ინფორმაციას (ტერმინალში).
+
+# 2
+
+age = int(input("Enter your age "))
+
+print(age + 7)
+
+salary = int(input("Enter your salary "))
+
+print(salary + 300)
+
+# 3
+
+food = input("Enter yor favourite food ")
+
+print(food)
+
+# 4
+
+name = input("Enter your name ")
+
+print("hello" +  name)
+
+# 5
+
+num = ("99")
+num1 = input("Enter any number")
+
+print(num + num1 )
+
+# 6
+
+# არასწორია
+
+# var = input("enter your age")
+# print(var + 3)
+
+# სწორია
+
+var = int(input("enter your age: "))
+print(var + 3)
+
+# 7
+
+user1 = int(input("enter number"))
+user2 = int(input("enter number"))
+sum = user1 // user2
+print(sum)
---
+++ day07/homework/homework.py
@@ -0,0 +1,32 @@
+# 1
+
+age = int(input("Enter your age! "))
+answer = age // 10
+print(answer)
+
+# 2
+
+num1 = int(input("Enter number: "))
+num2 = int(input("Enter number: "))
+answer = num1 * num2
+print(answer)
+
+# 3
+
+age = input("Enter your age: ")
+print(int(age))
+
+# 4
+
+num = int(input("Enter number: "))
+num1 = int(input("Enter number: "))
+num2 = int(input("Enter number: "))
+num3 = num + num1 + num2
+print(num3 // 3)
+
+# 5
+
+date = int(input("Enter your date of birth: "))
+date1 = 2025
+date2 = date1 - date
+print(date2)
