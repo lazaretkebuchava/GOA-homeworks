e715056eefb36031bf91ae9efc3c22bef45b4150
lazaretkebuchava <lazaretyebuchava@gmail.com>
2026-02-28 19:07:37 +0400

    commit

---
+++ day21/homework/homework.py
@@ -0,0 +1,15 @@
+# 1 
+
+# argument - არგუმენტი არის მნიშვნელობა,რომელსაც ფუნქციას გადავცემთ რადგან მან იმუშაოს.
+# function - ფუნქცია არის კოდის ბლოკი,რომელიც ასრულებს კონკრეტულ დავალებას.
+
+# 2
+
+# example I 
+print("Hello World!")
+# example II
+name = input("Enter Your Name! ")
+print(name)
+# example III
+numbers = [1, 2 , 3 , 4, 5, 6 , 7 , 8 , 10]
+print(len(numbers))
